a2d8cd370d948cf385eb35175f2006086ae590a0
Mario Jimenez <mario.aljim14@gmail.com>
2023-04-25 11:00:28 -0600

    Actualizacion Grupos

--- src/main/java/com/teamfour/sistutorias/presentation/GroupAdministrationController.java
+++ src/main/java/com/teamfour/sistutorias/presentation/GroupAdministrationController.java
@@ -1,4 +1,72 @@
 package com.teamfour.sistutorias.presentation;
 
 public class GroupAdministrationController {
+    @javafx.fxml.FXML
+    private Button btnCancel;
+    @javafx.fxml.FXML
+    private Button btnSaveGroup;
+    @javafx.fxml.FXML
+    private TextField tfNRC;
+    @javafx.fxml.FXML
+    private TableView tvGroup;
+    @javafx.fxml.FXML
+    private TableColumn tcPersonalNum;
+    @javafx.fxml.FXML
+    private TableColumn tcName;
+    @javafx.fxml.FXML
+    private Button btnDeleteGroup;
+    @javafx.fxml.FXML
+    private Button btnModifyGroup;
+    @javafx.fxml.FXML
+    private Label lbl_action;
+    @javafx.fxml.FXML
+    private Button btnCancelModification;
+    @javafx.fxml.FXML
+    private TableView tvTeacher;
+    @javafx.fxml.FXML
+    private TableColumn tcPersonalNum;
+    @javafx.fxml.FXML
+    private TableColumn tcNameTeacher;
+    @javafx.fxml.FXML
+    private TableView tvEE;
+    @javafx.fxml.FXML
+    private TableColumn tcEE;
+    @javafx.fxml.FXML
+    private TextField tfSelectedEE;
+    @javafx.fxml.FXML
+    private TextField tfSelectedTeacher;
+    @javafx.fxml.FXML
+    private ComboBox cbbAcademicProblem;
+
+    @javafx.fxml.FXML
+    public void cancel(ActionEvent actionEvent) {
+    }
+
+    @Deprecated
+    public void saveTeacher(ActionEvent actionEvent) {
+    }
+
+    @Deprecated
+    public void deleteTeacher(ActionEvent actionEvent) {
+    }
+
+    @Deprecated
+    public void modifyTeacher(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void cancelModification(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void saveGroups(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void deleteGroup(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void modifyGroup(ActionEvent actionEvent) {
+    }
 }
